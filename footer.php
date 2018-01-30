5d905bf08833b213f02225b5c3f1c064fb69ca26
fchristiansen <felipe@seo2.cl>
2018-01-30 18:40:27 -0300

    ajustes

--- footer.php
+++ footer.php
@@ -1,103 +1,104 @@
-		<footer>
-			<div class="container">
-				<form id="newsletter">
-					<div class="row">
-						<div class="box-news-letter clearfix">
-							<div class="col-sm-3">
-									<div class="texto-sobre">
-										<img  id="sobre" class="img-responsive" src="assets/img/sobre.png" alt="">
-										<p>Sign up for <br>
-										our newsletter</p>
-									</div>
+<footer>
+	<div class="container">
+		<form id="newsletter">
+			<div class="row">
+				<div class="box-news-letter clearfix">
+					<div class="col-sm-3">
+							<div class="texto-sobre">
+								<img  id="sobre" class="img-responsive" src="assets/img/sobre.png" alt="">
+								<p>Sign up for <br>
+								our newsletter</p>
 							</div>
-							<div class="col-sm-6">
-								<div class="form-group">
-										<label class="sr-only" for="email">Email address</label>
-										<input type="email" class="form-control" id="email" placeholder="Your Email Adress">
-								</div>
-							</div>
-							<div class="col-sm-3">
-								<button type="submit" class="btn btn-default bt-submit">submit</button>
-							</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group">
+								<label class="sr-only" for="email">Email address</label>
+								<input type="email" class="form-control" id="email" placeholder="Your Email Adress">
 						</div>
 					</div>
-				</form>
-					<div class="box-links-footer clearfix">
-						<div class="col-sm-3">
-								<h4>chile</h4>
-								<ul>
-									<li><a href="">Wine and Food Experience</a></li>
-									<li><a href="">Active Wine and Food Experience</a></li>
-									<li><a href="">Tailor-Made</a></li>
-									<li><a href="">Corporate and Press Trips</a></li>
-									<li><a href="">Wine Regions Map</a></li>
-									<li><a href="">Hotels</a></li>
-									<li><a href="">Terms and Conditions</a></li>
-								</ul>
-						</div>
-						<div class="col-sm-3">
-							<h4>argentina</h4>
-								<ul>
-									<li><a href="">Wine and Food Experience</a></li>
-									<li><a href="">Active Wine and Food Experience</a></li>
-									<li><a href="">Tailor-Made</a></li>
-									<li><a href="">Corporate and Press Trips</a></li>
-									<li><a href="">Wine Regions Map</a></li>
-									<li><a href="">Hotels</a></li>
-									<li><a href="">Terms and Conditions</a></li>
-								</ul>
-						</div>
-						<div class="col-sm-2">
-							<h4>(+) destinations</h4>
-								<ul>
-									<li><a href="">France</a></li>
-									<li><a href="">New Zealand</a></li>
-									<li><a href="">Uruguay</a></li>
-								</ul>
-						</div>
-						<div class="col-sm-2">
-							<h4>(+) about us</h4>
+					<div class="col-sm-3">
+						<button type="submit" class="btn btn-default bt-submit">submit</button>
+					</div>
+				</div>
+			</div>
+		</form>
+			<div class="box-links-footer clearfix">
+				<div class="row">
+					<div class="col-sm-3">
+							<h4>chile</h4>
 							<ul>
-								<li><a href="">History</a></li>
-								<li><a href="">Team</a></li>
-								<li><a href="">Testimonials</a></li>
-								<li><a href="">Press</a></li>
+								<li><a href="">Wine and Food Experience</a></li>
+								<li><a href="">Active Wine and Food Experience</a></li>
+								<li><a href="">Tailor-Made</a></li>
+								<li><a href="">Corporate and Press Trips</a></li>
+								<li><a href="">Wine Regions Map</a></li>
+								<li><a href="">Hotels</a></li>
+								<li><a href="">Terms and Conditions</a></li>
 							</ul>
-						</div>
-						<div class="col-sm-2">
-								<h4>contact</h4>
-								<ul>
-									<li><a href="">+569 927 81 331 </a></li>
-									<li><a href="">info@winetravelchile.com</a></li>
-									<li>&nbsp</li>
-									<li>&nbsp</li>
-									<li><a href="">SITE MAP</a></li>
-								</ul>
-						</div>
+					</div>
+					<div class="col-sm-3">
+						<h4>argentina</h4>
+							<ul>
+								<li><a href="">Wine and Food Experience</a></li>
+								<li><a href="">Active Wine and Food Experience</a></li>
+								<li><a href="">Tailor-Made</a></li>
+								<li><a href="">Corporate and Press Trips</a></li>
+								<li><a href="">Wine Regions Map</a></li>
+								<li><a href="">Hotels</a></li>
+								<li><a href="">Terms and Conditions</a></li>
+							</ul>
+					</div>
+					<div class="col-sm-2">
+						<h4>(+) destinations</h4>
+							<ul>
+								<li><a href="">France</a></li>
+								<li><a href="">New Zealand</a></li>
+								<li><a href="">Uruguay</a></li>
+							</ul>
+					</div>
+					<div class="col-sm-2">
+						<h4>(+) about us</h4>
+						<ul>
+							<li><a href="">History</a></li>
+							<li><a href="">Team</a></li>
+							<li><a href="">Testimonials</a></li>
+							<li><a href="">Press</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-2">
+						<h4>contact</h4>
+						<ul>
+							<li><a href="">+569 927 81 331 </a></li>
+							<li><a href="">info@winetravelchile.com</a></li>
+							<li>&nbsp</li>
+							<li>&nbsp</li>
+							<li><a href="">SITE MAP</a></li>
+						</ul>
 					</div>
 				</div>
+
 			</div>
-		</footer>
-		<div class="container-fluid no-pad clearfix">
-				<div class="container">
-					<div class="row">
-							<div class="col-sm-6">
-								<p class="copy">WINE TRAVEL CHILE. | 2018 ALL RIGHTS RESERVED.</p>
-							</div>
-							<div class="col-sm-6">
-								<div class="rrss">
-									<ul>
-										<li>Follow us:</li>
-										<li><a href="">Facebook</a></li>
-										<li><a href="">instagram</a></li>
-										<li><a href="">twitter</a></li>
-										<li><a href="">youtube</a></li>
-									</ul>
-								</div>
-							</div>
+		</div>
+	</div>
+</footer>
+<div class="container-fluid no-pad clearfix rrss-container">
+		<div class="container">
+			<div class="row">
+					<div class="col-sm-6">
+						<p class="copy">WINE TRAVEL CHILE. | 2018 ALL RIGHTS RESERVED.</p>
 					</div>
-				</div>
+					<div class="col-sm-6">
+						<ul class="rrss">
+							<li>Follow us:</li>
+							<li><a href="">Facebook</a></li>
+							<li><a href="">instagram</a></li>
+							<li><a href="">twitter</a></li>
+							<li><a href="">youtube</a></li>
+						</ul>
+					</div>
 			</div>
+		</div>
+	</div>
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="http://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->

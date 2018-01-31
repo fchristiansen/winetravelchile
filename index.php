60ff4c7e5bb402f59d95913de59f79cefee66f77
fchristiansen <felipe@seo2.cl>
2018-01-31 13:07:41 -0300

    desarrollo sub menu
    desarrollo sub menu y otros ajustes

--- index.php
+++ index.php
@@ -1,7 +1,72 @@
 <?php include('header.php'); ?>
 <?php include('main-nav.php'); ?>
 <?php include('slider-home.php'); ?>
+<section id="sub-menu">
+	<div class="container-fluid no-pad">
+		<div class="container">
+			<div class="row">
+				<div class="menu-experiences">
+					<div class="col-sm-3">
+						<div class="caluga">
 
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						    		<h3 class="caluga-title">  WINE AND FOOD EXPERIENCES</h3>
+						    	</div>
+						        <img class="caluga-img" src="assets/img/exp1.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="caluga">
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						      		<h3 class="caluga-title"> ACTIVE WINE AND WINE EXPERIENCE</h3>
+						      	</div>
+						        <img class="caluga-img" src="assets/img/exp2.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="caluga">
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						      		<h3 class="caluga-title">TAILOR - MADE</h3>
+						      	</div>
+						        <img class="caluga-img" src="assets/img/exp3.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="caluga">
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						      		<h3 class="caluga-title">CORPORATE AND PRESS TRIPS</h3>
+						      	</div>
+						        <img class="caluga-img" src="assets/img/exp4.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+		<div id="sub-menu-experiences-container">
+			<div class="container position-relative">
+				<div class="menu-list-wrapper">
+					<ul class="nav navbar-nav ">
+					<li class="active"><a href="">WINE REGIONS MAP</a></li>
+					<li>|</li>
+					<li><a href="">HOTELS</a></li>
+					<li>|</li>
+					<li><a href="">TERMS AND CONDITIONS</a></li>
+				</ul>
+				</div>
+
+			</div>
+		</div>
+</section>
 		<section id="intro" class="position-relative">
 			<div class="uva"></div>
 			<div class="parra"></div>
@@ -19,8 +84,9 @@
 								and lodging in small and luxury hotels.
 							</p>
 							<div class="box-more">
-								<a href="">more</a>
-								<img src="assets/img/arrow-more.png" alt="">
+								<a href="">more
+									<img src="assets/img/arrow-more.png" alt="">
+								</a>
 							</div>
 
 						</div>

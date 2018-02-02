8db1cf26ce9bb630f96f540a1bb0dc14b2990f25
fchristiansen <felipe@seo2.cl>
2018-02-02 18:11:44 -0300

    menu experiencias

---
+++ include-menu-experiencias.php
@@ -0,0 +1,70 @@
+<section id="sub-menu" class="hide">
+	<div class="container-fluid no-padding">
+		<div class="container">
+			<div class="row">
+				<div class="menu-experiences">
+					<div class="col-sm-3">
+						<div class="caluga">
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						    		<h3 class="caluga-title">  WINE AND FOOD EXPERIENCES</h3>
+						    	</div>
+						        <img class="caluga-img" src="assets/img/exp1.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="caluga">
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						      		<h3 class="caluga-title"> ACTIVE WINE AND WINE EXPERIENCE</h3>
+						      	</div>
+						        <img class="caluga-img" src="assets/img/exp2.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="caluga">
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						      		<h3 class="caluga-title">TAILOR - MADE</h3>
+						      	</div>
+						        <img class="caluga-img" src="assets/img/exp3.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="caluga">
+						    <a class="caluga-enlace" href="">
+						    	<div class="lente">
+						      		<h3 class="caluga-title">CORPORATE AND PRESS TRIPS</h3>
+						      	</div>
+						        <img class="caluga-img" src="assets/img/exp4.jpg" alt="">
+						    </a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+		<div id="sub-menu-experiences-container">
+			<div class="container position-relative">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="menu-list-wrapper">
+							<div class="text-center">
+								<ul class="">
+									<li class="active"><a href="">WINE REGIONS MAP</a></li>
+									<li class="divisor-item-sub-menu">|</li>
+									<li><a href="">HOTELS</a></li>
+									<li class="divisor-item-sub-menu">|</li>
+									<li><a href="">TERMS AND CONDITIONS</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</section>

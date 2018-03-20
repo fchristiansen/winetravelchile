8cb967be56ff157a758eb7ae20114c753f461003
fchristiansen <felipe@seo2.cl>
2018-03-20 12:19:08 -0300

    agregado tag manager y ajuste slider mobile
    esta comentado, descomentar al pasar a produccion

--- header.php
+++ header.php
@@ -7,6 +7,16 @@
 <!DOCTYPE html>
 <html lang="es">
 	<head>
+
+		<!-- Google Tag Manager -->
+
+<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N92D78P');</script> -->
+
+<!-- End Google Tag Manager -->
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -52,4 +62,8 @@
 		<link rel="icon" type="image/png" href="<?php bloginfo('template_url');?>/assets/img/logo.png" />
 	</head>
 	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N92D78P"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
+		<!-- End Google Tag Manager (noscript) -->
 		<div id="blogurl" class="hide" data-url="<?php bloginfo('template_url');?>"></div>
